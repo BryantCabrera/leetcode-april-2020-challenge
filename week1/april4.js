2c402e1c91621174fe6f96759b5e15c05e4eddcd
Bryant Cabrera <bcabrera@gocurrency.com>
2020-04-04 12:04:01 -0700

    Creates Day 4 solution.

--- week1/april4.js
+++ week1/april4.js
@@ -8,3 +8,22 @@
 
 // You must do this in -place without making a copy of the array.
 // Minimize the total number of operations.
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+	const indexOfZeros = [];
+	nums.forEach((num, index) => {
+		if (num === 0) {
+			indexOfZeros.push(index);
+		}
+	});
+
+	for (let i = indexOfZeros.length - 1; i >= 0; i--) {
+		nums.splice(indexOfZeros[i], 1);
+
+		nums.push(0);
+	}
+};
