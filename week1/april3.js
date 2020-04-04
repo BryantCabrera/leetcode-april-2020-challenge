a49041c2f11721d1d4f0b07d25cf195e11f8949d
Bryant Cabrera <bcabrera@gocurrency.com>
2020-04-03 18:25:59 -0700

    Creates Day 3 O(n^2) solution.

--- week1/april3.js
+++ week1/april3.js
@@ -9,3 +9,67 @@
 
 // If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 
+
+
+// O(n^2)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+	const sums = [];
+
+	// traverse the array
+	for (let i = 0; i < nums.length; i++) {
+		// cache the subset/subArray
+		let subarray = [];
+
+		// initiate set
+		// increase numbers in the set to sum
+		for (let j = i; j < nums.length; j++) {
+			subarray.push(nums[j]);
+
+			// sum them
+			const summedSubarray = subarray.reduce((acc, num) => {
+				return acc += num;
+			}, 0);
+
+			// store the sums in an array
+			sums.push(summedSubarray);
+		}
+
+		// re-initiate subarray
+		subarray = [];
+	}
+
+	// sort the sums array
+	sums.sort(function (a, b) {
+		return a - b;
+	});
+
+	// return the last element of the array
+	return sums[sums.length - 1];
+
+
+
+	// cache the subset/subArray
+
+	// initiate set
+
+	// increase numbers in the set to sum
+
+	// sum them
+
+	// store the sums in an array => // store the pair in a map where sum: subset/subArray
+
+	// reinitiate subarray
+
+	// sort the sums array => // compare the keys of this map
+
+	// return the last element of the array => // return the subset/subarray with the largest sum
+};
+
+
+
+
+// O(n)
