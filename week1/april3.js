ecbcc8288dcffd5459c0ca5ea7bd9c1dfa773dfd
Bryant Cabrera <bcabrera@gocurrency.com>
2020-04-03 19:34:25 -0700

    Creates Day 3 O(n) solution.

--- week1/april3.js
+++ week1/april3.js
@@ -69,8 +69,11 @@ var maxSubArray = function (nums) {
 	// return the last element of the array => // return the subset/subarray with the largest sum
 };
 
-
-
+///////////
+///////////
+///////////
+///////////
+///////////
 
 // O(n^2)
 /**
@@ -124,4 +127,65 @@ var maxSubArray = function (nums) {
 	// sort the sums array => // compare the keys of this map
 
 	// return the last element of the array => // return the subset/subarray with the largest sum
+};
+
+///////////
+///////////
+///////////
+///////////
+///////////
+
+// O(n)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+	// initialize the sum
+	let maxSum = 0;
+	let currentSum = 0;
+
+	// traverse the array
+	for (let i = 0; i < nums.length; i++) {
+
+		// if the currentSum + the current number in the loop is less than or equal to 0, 
+		// reinitialize sum to current number
+		if (currentSum + nums[i] <= 0) {
+			currentSum = 0;
+			console.log(currentSum, 'currentSum reinitiated');
+		} else {
+			// if the currentSum + the current number in the loop is positive, continue adding
+			currentSum += nums[i];
+			if (currentSum > maxSum) {
+				maxSum = currentSum;
+			}
+		}
+	}
+
+	// Edge Cases
+	// all positive #s in the array
+	let containsAllPositiveNums = true;
+	nums.map(num => {
+		if (num / Math.abs(num) === -1) containsAllPositiveNums = false;
+	});
+	if (containsAllPositiveNums) {
+		return nums.reduce((acc, num) => {
+			return acc += num;
+		}, 0);
+	}
+
+	// all negative #s in the array
+	let containsAllNegativeNums = true;
+	nums.map(num => {
+		if (num / Math.abs(num) !== -1) containsAllNegativeNums = false;
+	});
+	if (containsAllNegativeNums) {
+		const sortedArr = nums.sort(function (a, b) {
+			return a - b;
+		});
+		return sortedArr[sortedArr.length - 1];
+	}
+
+	// return the sum
+	return maxSum;
 };
