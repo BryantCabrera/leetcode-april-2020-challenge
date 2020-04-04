6b85d8978de642061bae4b3ab94a024310578c4f
Bryant Cabrera <bcabrera@gocurrency.com>
2020-04-03 18:43:56 -0700

    Creates Day 3 O(n^2) solution.

--- week1/april3.js
+++ week1/april3.js
@@ -72,4 +72,56 @@ var maxSubArray = function (nums) {
 
 
 
-// O(n)
+// O(n^2)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+	const sums = [];
+
+	// traverse the array
+	for (let i = 0; i < nums.length; i++) {
+		// cache the current sum
+		let currentSum = 0;
+
+		// initiate set
+		// increase numbers in the set to sum
+		for (let j = i; j < nums.length; j++) {
+			// sum them
+			currentSum += nums[j];
+
+			// store the sums in an array
+			sums.push(currentSum);
+		}
+
+		// re-initiate subarray
+		subarray = [];
+	}
+
+	// sort the sums array
+	sums.sort(function (a, b) {
+		return a - b;
+	});
+
+	// return the last element of the array
+	return sums[sums.length - 1];
+
+
+
+	// cache the subset/subArray
+
+	// initiate set
+
+	// increase numbers in the set to sum
+
+	// sum them
+
+	// store the sums in an array => // store the pair in a map where sum: subset/subArray
+
+	// reinitiate subarray
+
+	// sort the sums array => // compare the keys of this map
+
+	// return the last element of the array => // return the subset/subarray with the largest sum
+};
