65461cccad4f808974fd2b6d3c0166fdf8254472
Bryant Cabrera <bcabrera@gocurrency.com>
2020-04-06 21:10:12 -0700

    Creates Day 6 solution.

--- week1/april6.js
+++ week1/april6.js
@@ -23,13 +23,29 @@ var groupAnagrams = function (strs) {
 	const anagramMap = {};
 
 	// loop through list of words
+	strs.forEach(string => {
+		// cache current word
+		// const currentWord = string;
 
-	// cache current word
-	// split that string into an array of letters, sort the array, join letters back into a string
+		// split that string into an array of letters, sort the array, join letters back into a string
+		const sortedWord = string.split('').sort().join('');
 
-	// create a map where {sortedWord: [originalWord]}
-	// if that sortedWord key already exists in the map, push the unsorted word into that key's array
-	// if that sortedWord does NOT key already exists in the map, initialize the key: value pair and push the unsorted word into that key's array
+		// create a map where {sortedWord: [originalWord]}
+		// if that sortedWord key already exists in the map, push the unsorted word into that key's array
+		if (anagramMap[sortedWord]) {
+			anagramMap[sortedWord].push(string);
+		} else {
+			// if that sortedWord does NOT key already exists in the map, initialize the key: value pair and push the unsorted word into that key's array
+			anagramMap[sortedWord] = [string];
+		}
+	});
 
-	// return the reduced map to match the shape of the output in problem description
+
+	// return the translated map to match the shape of the output in problem description
+	const solution = [];
+	for (const group in anagramMap) {
+		solution.push(anagramMap[group]);
+	}
+
+	return solution;
 };
