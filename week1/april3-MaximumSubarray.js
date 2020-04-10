ec1214977bf19c1ea5da210accb4917cc6c51624
Bryant Cabrera <bcabrera@gocurrency.com>
2020-04-09 22:11:30 -0700

    Removes commented out code.

--- week1/april3-MaximumSubarray.js
+++ week1/april3-MaximumSubarray.js
@@ -52,24 +52,6 @@ var maxSubArray = function (nums) {
 
 	// return the last element of the array
 	return sums[sums.length - 1];
-
-
-
-	// cache the subset/subArray
-
-	// initiate set
-
-	// increase numbers in the set to sum
-
-	// sum them
-
-	// store the sums in an array => // store the pair in a map where sum: subset/subArray
-
-	// reinitiate subarray
-
-	// sort the sums array => // compare the keys of this map
-
-	// return the last element of the array => // return the subset/subarray with the largest sum
 };
 
 ///////////
@@ -112,24 +94,6 @@ var maxSubArray = function (nums) {
 
 	// return the last element of the array
 	return sums[sums.length - 1];
-
-
-
-	// cache the subset/subArray
-
-	// initiate set
-
-	// increase numbers in the set to sum
-
-	// sum them
-
-	// store the sums in an array => // store the pair in a map where sum: subset/subArray
-
-	// reinitiate subarray
-
-	// sort the sums array => // compare the keys of this map
-
-	// return the last element of the array => // return the subset/subarray with the largest sum
 };
 
 ///////////
