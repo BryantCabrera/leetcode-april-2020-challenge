fb7eee14019e894b2980ecb8b7b77f620dcdd4cc
BryantCabrera <cabrera.bryant@gmail.com>
2020-04-21 21:37:40 -0700

    Adds Day 21 problem.

--- week3/april20-ConstructBinarySearchTree.js
+++ week3/april20-ConstructBinarySearchTree.js
@@ -18,3 +18,84 @@
 
 // 1 <= preorder.length <= 100
 // The values of preorder are distinct.
+
+
+
+
+
+// Unused Solutions
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val) {
+//  *     this.val = val;
+//  *     this.left = this.right = null;
+//  * }
+//  */
+// /**
+//  * @param {number[]} preorder
+//  * @return {TreeNode}
+//  */
+// var bstFromPreorder = function(preorder) {
+// 	console.log(preorder);
+
+// 	class TreeNode {
+// 			constructor (val) {
+// 					this.val = val;
+// 					this.left = this.right = null;
+// 			} 
+// 	}
+	
+// 	const rootNode = new TreeNode(preorder[0]);
+// 	console.log(rootNode, 'root');
+	
+// 	let currentNode = rootNode;
+// 	for (let i = 1; i < preorder.length; i++) {
+// 			if (preorder[i] < currentNode.val) {
+// 					currentNode.left = new TreeNode(preorder[i]);
+// 			} else if (preorder[i] > currentNode.val) {
+// 					currentNode.right = new TreeNode(preorder[i]);
+// 			}
+// 	}
+// };
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val) {
+//  *     this.val = val;
+//  *     this.left = this.right = null;
+//  * }
+//  */
+// /**
+//  * @param {number[]} preorder
+//  * @return {TreeNode}
+//  */
+// var bstFromPreorder = function(preorder) {
+// 	let root = new TreeNode(preorder[0]);
+	
+// 	const s = [];
+	
+// 	s.push(root);
+	
+// 	let i = 1;
+	
+// 	while(i < preorder.length){
+// 			let temp = null;
+
+// 			while (s && preorder[i] > s[s.length - 1].val){
+// 					temp = s.pop();
+// 			}
+
+// 			if (temp != null){
+// 					temp.right = TreeNode(preorder[i]);
+// 					s.push(temp.right);
+// 			} else {
+// 					temp = s[s.length - 1];
+// 					temp.left = TreeNode(preorder[i]);
+// 					s.push(temp.left);
+// 			}
+// 			i++;
+
+// 			return root;
+// 	}
+// };
