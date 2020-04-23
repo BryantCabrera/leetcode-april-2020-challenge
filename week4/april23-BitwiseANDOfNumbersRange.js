cf875bbce273dca26c4e98752ebae90810367260
BryantCabrera <cabrera.bryant@gmail.com>
2020-04-23 16:00:43 -0700

    Adds Day 23 problem.

---
+++ week4/april23-BitwiseANDOfNumbersRange.js
@@ -0,0 +1,15 @@
+// Bitwise AND of Numbers Range
+// https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3308/
+
+// Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+// Example 1:
+
+// Input: [5,7]
+// Output: 4
+// Example 2:
+
+// Input: [0,1]
+// Output: 0
+
+
