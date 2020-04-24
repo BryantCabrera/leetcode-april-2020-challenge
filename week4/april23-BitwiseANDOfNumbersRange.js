767d3747db628ea4cb9797a36cf2a795a192786f
BryantCabrera <cabrera.bryant@gmail.com>
2020-04-23 18:06:39 -0700

    Creates Day 23 solution.

--- week4/april23-BitwiseANDOfNumbersRange.js
+++ week4/april23-BitwiseANDOfNumbersRange.js
@@ -13,3 +13,57 @@
 // Output: 0
 
 
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var rangeBitwiseAnd = function(m, n) {
+	let count = 0;
+	
+	while (m !== n) {
+			m = m >> 1;
+			
+			n = n >> 1;
+			
+			count++;
+	}
+	
+	return n << count;
+};
+
+
+// Without transforming Number
+// /**
+//  * @param {number} m
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var rangeBitwiseAnd = function(m, n) {
+// 	let temp = m;
+	
+// 	for (let i = m + 1; i <= n; i++) {
+// 			temp = temp & i;
+// 	}
+	
+// 	return temp;
+// };
+
+
+// Transformation into bit
+// /**
+//  * @param {number} m
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var rangeBitwiseAnd = function(m, n) {
+// 	let temp = (m >>> 0).toString(2);
+	
+// 	for (let i = m + 1; i <= n; i++) {
+// 			console.log(parseInt(temp, 2), 'parseInt temp');
+// 			temp = (parseInt(temp, 2) & i >>> 0).toString(2);
+// 			console.log(temp, 'temp');
+// 	}
+	
+// 	return parseInt(temp, 2);
+// };
