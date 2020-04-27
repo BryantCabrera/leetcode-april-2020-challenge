3c1fb421ffa97ff24f61da32ae07cd66703781dd
BryantCabrera <cabrera.bryant@gmail.com>
2020-04-26 20:13:00 -0700

    Adds Day 26 solution.

--- week4/april26-LongestCommonSubsequence.js
+++ week4/april26-LongestCommonSubsequence.js
@@ -33,3 +33,29 @@
 // 1 <= text1.length <= 1000
 // 1 <= text2.length <= 1000
 // The input strings consist of lowercase English characters only.
+
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+	let test1Arr = new Array(text1.length+1);
+	
+	for (let i = 0; i <= text1.length; i++) {
+			test1Arr[i]=(new Array(text2.length + 1)).fill(0);
+	}
+			
+	for (let i = 1; i <= text1.length; i++){
+			for(let j = 1; j <= text2.length; j++){
+					if (text1[i-1] === text2[j-1]){
+							test1Arr[i][j] = 1 + test1Arr[i-1][j-1];
+					} else {
+							test1Arr[i][j] = Math.max(test1Arr[i][j-1], test1Arr[i-1][j]);
+					}
+							
+			}
+	}
+	return test1Arr[text1.length][text2.length];
+};
