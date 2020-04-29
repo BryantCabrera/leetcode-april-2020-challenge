55cd680de25203071933d5dfa0125cd2c73ff21e
BryantCabrera <cabrera.bryant@gmail.com>
2020-04-29 09:46:04 -0700

    Creates Day 29 solution.

--- week5/april29-BinaryTreeMaximumPathSum.js
+++ week5/april29-BinaryTreeMaximumPathSum.js
@@ -26,3 +26,46 @@
 
 // Output: 42
 
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+	// Initialize a cache to a very small number to account for a path with a negative sum
+	let max = -Infinity 
+	
+	// Recursively traverse the binary tree 
+	function getMaxSum(tree) {
+			// Termination Condition: Once we have reached a null value, we have reached the end of the branch
+			if (tree == null) { 
+					return 0; 
+			}
+			
+			// Calculate the root to leaf sum where root is the left node
+			const leftBranch = Math.max(0, getMaxSum(tree.left)) ;
+			
+			// Calculate the root to leaf sum where root is the right node
+			const rightBranch = Math.max(0, getMaxSum(tree.right));
+			
+			// Sum the path: left -> root -> right (leaf to leaf)
+			const currentPath = leftBranch + tree.val + rightBranch  
+			
+			// Set the value of max to be the larger of current max or the current path sums
+			max = Math.max(max, currentPath);
+			
+			return tree.val + Math.max(leftBranch, rightBranch);
+	}
+	
+	getMaxSum(root);
+	
+	return max; 
+};
